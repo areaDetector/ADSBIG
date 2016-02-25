f8c29d21215f2e87bf22f951cfb0f919e9592830
Matthew Pearson <pearsonmr@ornl.gov>
2016-02-25 11:43:54 -0500

    example: exapand test script to change the readout mode, exposure time and take dark field images.

--- example/test_script.py
+++ example/test_script.py
@@ -7,6 +7,9 @@ import time
 from epics import caget, caput
 
 base_pv = "BL99:Det:SBIG:"
+exposures = [0.1, 1, 10]
+readout_modes = [0, 1, 2]
+images = 20
 
 def main():
     
@@ -14,14 +17,17 @@ def main():
 
     print "Current detector state: " + str(caget(base_pv+"StatusMessage_RBV", as_string=True))
 
-    caput(base_pv+"AcquireTime", 1, wait=True)
+    for readout in readout_modes: 
+        print "ReadoutMode: " + str(readout)
+        caput(base_pv+"ReadoutMode", readout, wait=True)
 
-    for i in range(3):
-        print "ReadoutMode: " + str(i)
-        caput(base_pv+"ReadoutMode", i, wait=True)
+        for exposure in exposures:
+            print "AcquireTime: " + str(exposure)
+            caput(base_pv+"AcquireTime", exposure, wait=True)
 
-        for image in range(10):
-            print "Acquire: " + str(i)
+            #Take a dark field
+            print "Taking a dark field..."
+            caput(base_pv+"DarkField", 1, wait=True)
             caput(base_pv+"Acquire", 1, wait=True, timeout=20)
             status = caget(base_pv+"DetectorState_RBV")
             if (status != 0):
@@ -29,6 +35,20 @@ def main():
                 print base_pv+"DetectorState_RBV="+str(status)
                 print str(caget(base_pv+"StatusMessage_RBV", as_string=True))
                 sys.exit(1)
+            print "Done."
+            caput(base_pv+"DarkField", 0, wait=True)
+
+            #Take light fields
+            print "Taking " + str(images) + " images..."
+            for image in range(images):
+                print "Acquire: " + str(image)
+                caput(base_pv+"Acquire", 1, wait=True, timeout=20)
+                status = caget(base_pv+"DetectorState_RBV")
+                if (status != 0):
+                    print "ERROR!"
+                    print base_pv+"DetectorState_RBV="+str(status)
+                    print str(caget(base_pv+"StatusMessage_RBV", as_string=True))
+                    sys.exit(1)
 
     print "Complete."
     sys.exit(0)
